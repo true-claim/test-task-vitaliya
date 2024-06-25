a30c4c600346bdcfb33f983e4f1db057220ef0cc
Павел Горбунов <pavelgorbunov@MacBook-Air-Pavel.local>
2024-06-25 15:27:33 +0400

    add readme & function create table

--- src/main.js
+++ src/main.js
@@ -10,20 +10,13 @@ const logger = makeLogger('main');
 const port = 3020;
 const app = express();
 
-app.use(express.json());
-app.use('/api', balanceRouter);
-app.listen(port, () => logger.info(`Server run http://localhost:${port}`));
+async function main() {
+    app.use(express.json());
+    app.use('/api', balanceRouter);
+    app.listen(port, () => logger.info(`Server run http://localhost:${port}`));
 
+    await syncModels();
+    await createDefaultUser()
+}
 
-// (async () => {
-    // try {
-        
-    // } catch (error) {
-        
-    // }
-//     app.use(express.json())
-//     app.use('/api', balanceRouter)
-//     app.listen(port, () => console.log(`Server run http://localhost:${port}`))
-      //  await syncModels();
-//     await createDefaultUser()
-// })();
+main()
